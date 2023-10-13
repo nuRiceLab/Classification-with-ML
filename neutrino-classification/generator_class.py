00f957f4abcb926e07149eb13ebe6e93bc709321
Aaron Higuera <aaronhiguera@aarons-air.dyndns.rice.edu>
2023-10-12 19:00:46 -0500

    remove normalization

--- neutrino-classification/generator_class.py
+++ neutrino-classification/generator_class.py
@@ -10,16 +10,9 @@ class DataGenerator(keras.utils.Sequence):
         self.batch_size = batch_size
         self.dim = dim
         self.n_channels = n_channels
-        
-    def __normalize__(self, data):
-        #Takes input of numpy array
-        if not isinstance(data, np.ndarray):
-            raise Exception('Must use array as input')
-        if np.amax(data) == 0:
-            return np.zeros(np.shape(data))
-        data_range = np.amax(data)-np.amin(data)
-        return (data - np.amin(data))/data_range
-        
+
+    # __normalize__ method removed
+    
     def get_info(self, file):
         with open(file, 'rb') as info_file:
             info = info_file.readlines()
@@ -35,7 +28,7 @@ class DataGenerator(keras.utils.Sequence):
         planes = self.dim[1]
         views = self.n_channels
         file = open(file_name, 'rb').read()
-        pixels_map = np.frombuffer(zlib.decompress(file), dtype=np.uint8 )
+        pixels_map = np.frombuffer(zlib.decompress(file), dtype=np.uint8)
         pixels_map = pixels_map.reshape(views, planes, cells)
         return pixels_map
     
@@ -52,16 +45,16 @@ class DataGenerator(keras.utils.Sequence):
                 truth_label = 2
             elif pdg == 16:
                 truth_label = 3
-            image = file[:-5] +'.gz'
+            image = file[:-5] + '.gz'
             data_maps.append(self.get_pixels_map(image))
             data_labels.append(truth_label)
         return data_maps, data_labels
     
     def __len__(self):
-         return int(np.floor(len(self.files) / self.batch_size))
-        
+        return int(np.floor(len(self.files) / self.batch_size))
+    
     def __getitem__(self, index):
-        indexes = list(range(index*self.batch_size, (index+1)*self.batch_size))
+        indexes = list(range(index * self.batch_size, (index + 1) * self.batch_size))
         
         files_temp = [self.files[i] for i in indexes]
         
@@ -73,11 +66,11 @@ class DataGenerator(keras.utils.Sequence):
         train_temp = []
         if self.n_channels == 1:
             for i in range(len(maps_z_view)):
-                train_temp.append(self.__normalize__(maps_z_view[i][0]))
+                train_temp.append(maps_z_view[i][0])
         
         elif self.n_channels == 3:
             for i in range(len(maps_z_view)):
-                train_temp.append(np.dstack((self.__normalize__(maps_u_view[i][0]), self.__normalize__(maps_v_view[i][0]), self.__normalize__(maps_z_view[i][0]))))
+                train_temp.append(np.dstack((maps_u_view[i][0], maps_v_view[i][0], maps_z_view[i][0])))
         train_temp = np.array(train_temp).reshape([self.batch_size, self.dim[0], self.dim[1], self.n_channels])
         labels_temp = np.array(labels_temp)
         X, y = self.__data_generation(train_temp, labels_temp)
@@ -88,8 +81,9 @@ class DataGenerator(keras.utils.Sequence):
         self.indexes = np.arange(len(self.files))
     
     def __data_generation(self, train_temp, labels_temp):
-        print
         return train_temp, labels_temp
-    
-        
-    
+
+
+
+
+
